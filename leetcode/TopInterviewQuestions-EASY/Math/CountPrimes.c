8a52f1f631d478c265694d5983a84e4b1818334e
lxr <lxr@email.com>
2018-10-16 21:42:23 +0800

    add CountPrimes.c

---
+++ leetcode/TopInterviewQuestions-EASY/Math/CountPrimes.c
@@ -0,0 +1,57 @@
+/*
+
+Count Primes
+  Go to Discuss
+Count the number of prime numbers less than a non-negative number, n.
+
+Example:
+
+Input: 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+
+*/
+
+int isPrime(int n)
+{
+	for(int i= 2; i*i <= n; i++)
+	{
+		if(n % i == 0)
+			return 0;
+	}
+	return 1;
+}
+
+int countPrimes(int n) 
+{
+	int count = 0;
+	for(int i = 2; i < n; i++)
+	{
+		if(isPrime(i) == 1)
+			count++;
+	}
+	return count;
+}
+
+
+//高效率
+
+int countPrimes(int n){
+    if(n<=2)
+        return 0;
+    
+    bool *isPrime=malloc(sizeof(bool)*n);
+    memset(isPrime, true, n);
+    
+    int primeCount = 1;
+    
+    for(int i=3; i<n; i+=2){
+        if(isPrime[i]){
+            primeCount++;
+            for(int j=i;j<n;j+=i){
+                isPrime[j]=false;
+            }
+        }
+    }
+     return primeCount;
+}
